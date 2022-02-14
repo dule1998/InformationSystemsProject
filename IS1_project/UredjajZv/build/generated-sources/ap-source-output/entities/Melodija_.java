e650ecfff38fc57f6c617720d05694452dde870f
dule1998 <80540056+dule1998@users.noreply.github.com>
2022-02-14 19:49:51 +0100

    Add files via upload

---
+++ IS1_project/UredjajZv/build/generated-sources/ap-source-output/entities/Melodija_.java
@@ -0,0 +1,18 @@
+package entities;
+
+import entities.AlarmNavijen;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.0.v20170811-rNA", date="2021-07-05T19:00:22")
+@StaticMetamodel(Melodija.class)
+public class Melodija_ { 
+
+    public static volatile SingularAttribute<Melodija, Integer> idM;
+    public static volatile SingularAttribute<Melodija, String> naziv;
+    public static volatile SingularAttribute<Melodija, String> lokacija;
+    public static volatile ListAttribute<Melodija, AlarmNavijen> alarmNavijenList;
+
+}
